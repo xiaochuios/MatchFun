2c9e021516bb2d1cedb032361b62dd3456b01598
Hung Yinkam <hongxianqin@hotmail.com>
2013-12-08 20:08:01 +0800

    Initial Commit

---
+++ MarchFun/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  MarchFun
+//
+//  Created by Apple on 8/12/13.
+//  Copyright hku.cs.msccs 2013年. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ MarchFun/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MarchFun
+//
+//  Created by Apple on 8/12/13.
+//  Copyright hku.cs.msccs 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
